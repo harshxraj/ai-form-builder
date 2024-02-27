77265f0e1c9fb4075fedc7976d3ad6f1c14961e0
harshxraj <harshraj0588@gmail.com>
2024-02-28 03:06:44 +0530

    dashboard ui done

---
+++ src/app/(admin)/view-forms/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import FormList from "@/app/forms/FormList";
+import { getUserForms } from "@/app/actions/getUserForms";
+import { InferSelectModel } from "drizzle-orm";
+import { forms as dbForms } from "@/db/schema";
+
+type Props = {};
+
+const page = async (props: Props) => {
+  const forms: InferSelectModel<typeof dbForms>[] = await getUserForms();
+  return (
+    <div>
+      <h1 className="text-4xl font-bold px-4 m-5">My Forms</h1>
+      <FormList forms={forms} />
+    </div>
+  );
+};
+
+export default page;
